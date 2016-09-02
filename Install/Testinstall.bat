2f8be41b0667daa13f65b0115aec120ff3adae33
Ralph Muehleisen <rmuehleisen@anl.gov>
2016-09-02 13:22:00 -0500

    Updated testinstall to include all tests

--- Install/Testinstall.bat
+++ Install/Testinstall.bat
@@ -1,14 +1,14 @@
 @ECHO *** BCUS Installation Test ***
 @ECHO This batch file will test your installation of Ruby, R, and BCUS:
 
-REM ruby -S UA.rb  test.osm test.epw --numLHS 3  --seed 1
+ruby -S UA.rb  test.osm test.epw --numLHS 3  --seed 1
 @echo.
 
-REM ruby -S SA.rb test.osm test.epw --morrisR 2 --morrisL 5  --seed 1
+ruby -S SA.rb test.osm test.epw --morrisR 2 --morrisL 5  --seed 1
 
 @echo.
 ruby -S BC_Setup.rb test.osm test.epw --numLHS 6  --seed 1
 
 @echo.
 
-REM ruby -S BC.rb test.osm test.epw --numMCMC 30 --numBurnin 3 
+ruby -S BC.rb test.osm test.epw --numMCMC 30 --numBurnin 3 
