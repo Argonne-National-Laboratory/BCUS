d74559f7c0b72d21068b86a6966e7470a3b0fa14
Ralph Muehleisen <rmuehleisen@anl.gov>
2018-07-11 15:51:40 -0500

    Converted test.osm to V2.5.0
    converted test.osm to v 2.5.0

--- Install/install_ruby_gems.bat
+++ Install/install_ruby_gems.bat
@@ -3,6 +3,6 @@ REM Install the ruby gems.  Use call because gem is a batch file itself
 REM Need to install nokogiri v 1.6.8.1 first because it's the last version that works with ruby 2.0.0
 REM but works with all current versions of rubyXL
 echo Installing nokogiri v1.6.8.1 and rubyXL
-call gem install nokogiri -v 1.6.8.1
+REM call gem install nokogiri -v 1.6.8.1
 call gem install rubyXL
 
